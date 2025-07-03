c89a8ed139e82f57a8a9396b27ee5a957759b7f5
OMS-STUDIO <omer121909@gmail.com>
2025-07-03 03:54:22 +0200

    main_updated.py

--- main_updated.py
+++ main_updated.py
@@ -44,6 +44,6 @@ def serve(path):
         else:
             return "index.html not found", 404
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port, debug=False)
